186551291529e3b13ce7125b5df416036b91d69e
YuXiangLo <linus13514@gmail.com>
2025-03-19 23:27:30 +0800

    refactor: remove redundant file

--- hw1/Blip.py
+++
@@ -1,5 +0,0 @@
-# Load model directly
-from transformers import AutoProcessor, AutoModelForImageTextToText
-
-processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-model = AutoModelForImageTextToText.from_pretrained("Salesforce/blip-image-captioning-base")
--- hw1/Phi4.py
+++
@@ -1,3 +0,0 @@
-# Load model directly
-from transformers import AutoModelForCausalLM
-model = AutoModelForCausalLM.from_pretrained("microsoft/Phi-4-multimodal-instruct", trust_remote_code=True)
